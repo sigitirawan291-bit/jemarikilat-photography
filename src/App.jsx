87e7c5be81a9daf8d3f94b0cd2c98a339a097414
Jemari Kilat <jemarikilat@gmail.com>
2026-08-07 20:36:33 +0700

    cleanup_unused_pages_and_publish_to_vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import VideoModal from './components/VideoModal';
-import BookingModal from './components/BookingModal';
-import LiveChatWidget from './components/LiveChatWidget';
-import { DataProvider, useData } from './context/DataContext';
-
-import HomeView from './views/HomeView';
-import GalleryView from './views/GalleryView';
-import VideoView from './views/VideoView';
-import EventView from './views/EventView';
-import PricingView from './views/PricingView';
-import ContactView from './views/ContactView';
-import AdminView from './views/AdminView';
-import FgDashboardView from './views/FgDashboardView';
+import { DataProvider } from './context/DataContext';
 import StudioToolsView from './views/StudioToolsView';
 
 function AppContent() {
-  const [activePage, setActivePage] = useState('studio-tools');
-
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900 flex flex-col justify-between selection:bg-blue-600 selection:text-white font-sans">
       
